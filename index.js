ef6f6c464c625987a069004917910ed76104b36f
Yahya Emre Cetinkaya <pib.yahya.cetinkaya@htwsaar.de>
2021-05-14 15:57:57 +0200

    Sequelize User model and signin method added

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
-app.listen(PORT,()=>{
-    console.log(`Server ${PORT} is listened!`)
+const db = require('./models');
+const {User} = require('./models');
+const cors = require('cors');
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('Hello')
 })
-app.get("/hello",(req,res)=>{
-     res.json({message:"hi"});
-})
+app.post("/signin",(req,res)=>{
+    const payload = {
+        name:req.body.name,
+        email:req.body.email,
+        googleId:req.body.googleId
+    }
+    console.log(req.body)
+    User.create(payload).then((res=>{
+        res.send('inserted new user to the database')
+    })).catch(err=>{
+        res.json({
+            error:err
+        })
+    }) 
+});
+db.sequelize.sync().then((req)=>{
+    app.listen(PORT,()=>{
+        console.log(`Server ${PORT} is listened!`)
+    })
+});
